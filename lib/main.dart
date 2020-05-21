3acd59e3385899d79052612fcd363b3683d16854
AniketSindhu <sindhuaniket@gmail.com>
2020-05-22 02:06:06 +0530

    google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
